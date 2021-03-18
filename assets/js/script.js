1e5002473166d99883856afc2f089d55a10898b7
Karen Roh <karenroh@hotmail.com>
2021-03-18 11:56:33 -0400

    started color coding timeblocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,55 +1,69 @@
 // Display current day at top of calendar
 var today = moment();
-$("#currentDay").text(today.format("dddd, MMMM Do"));
+var currentDay = today.format("dddd, MMMM Do");
+$("#currentDay").text(currentDay);
 
 // color coding timeblocks
-// past is white, present is coral, future is green
+// past is gray, present is red, future is green
+//if current time > printed time, add class future to make green color
+//if current time < printed time, add class past to make gray color
+//if current time === printed time, add class present to make red
+//can use isBefore, isSame, isAfter functions
+// function compareTime() {
+var currentHour = moment().hour();
+var hourArray = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+// console.log(currentHour);
+for (i = 0; i < hourArray.length; i++) {
+  if (currentHour === hourArray[i]) {
+    //present
+    console.log(hourArray[i]);
+  } else if (currentHour < hourArray[i]) {
+    //future
+    console.log(hourArray[i]);
+  }
+}
 
-// saving inputs to local storage as string
-var 
-// var student = document.getElementById("student-names"); 
-// var grade = document.getElementById("grades");
-// var comment = document.getElementById("msg");
-// var saveButton = document.getElementById("save");
-// var savedName = document.getElementById("saved-name");
-// function saveLastGrade() {
-//   // Save related form data as an object
-//   var studentGrade = {
-//     student: student.value,
-//     grade: grade.value,
-//     comment: comment.value.trim()
-//   };
-//   // Use .setItem() to store object in storage and JSON.stringify to convert it as a string
-//   localStorage.setItem("studentGrade", JSON.stringify(studentGrade));
-// }
-// function renderLastGrade() {
-//   // Use JSON.parse() to convert text to JavaScript object
-//   var lastGrade = JSON.parse(localStorage.getItem("studentGrade"));
-//   // Check if data is returned, if not exit out of the function
-//   if (lastGrade !== null) {
-//   document.getElementById("saved-name").innerHTML = lastGrade.student;
-//   document.getElementById("saved-grade").innerHTML = lastGrade.grade;
-//   document.getElementById("saved-comment").innerHTML = lastGrade.comment;
-//   } else {
-//     return;
+$("#hour-9").addClass("past");
+$("#hour-10").addClass("past");
+$("#hour-11").addClass("present");
+$("#hour-12").addClass("future");
+$("#hour-1").addClass("future");
+$("#hour-2").addClass("future");
+$("#hour-3").addClass("future");
+$("#hour-4").addClass("future");
+$("#hour-5").addClass("future");
+//     }
 //   }
 // }
-// saveButton.addEventListener("click", function(event) {
-// event.preventDefault();
-// saveLastGrade();
-// renderLastGrade();
-// });
-// // The init() function fires when the page is loaded 
-// function init() {
-//   // When the init function is executed, the code inside renderLastGrade function will also execute
-//   renderLastGrade();
-// }
-// init();
+// saving inputs to local storage as string when save button clicked
+const saveButton = $(".saveBtn");
+saveButton.on("click", function (event) {
+  event.preventDefault();
+  saveLastEvent();
+  renderLastEvent();
+});
 
-
-
-
-
+// need to make .description more specific to the timeblock
+// right now it saves changes to ALL timeblocks
+var eventInput = $(".description");
+// console.log(eventInput);
+function saveLastEvent() {
+  var eventDescription = {
+    eventInput: eventInput.val(),
+  };
+  localStorage.setItem("eventDescription", JSON.stringify(eventDescription));
+}
+function renderLastEvent() {
+  var lastEvent = JSON.parse(localStorage.getItem("eventDescription"));
+  if (lastEvent !== null) {
+    eventInput.text(lastEvent.eventInput);
+  }
+}
+function init() {
+  // When the init function is executed, the code inside renderLastEvent function will also execute
+  renderLastEvent();
+}
+init();
 
 // need to do:
 // 1. color code timeblocks to indicate whether it is in the past, present, or future
